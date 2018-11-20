c16d0fc9c1c96a832a014641882cc8b3c105bf9e
cochiletSerban <cochiletSerban@gmail.com>
2018-11-20 22:23:48 +0200

    fixed https bug in get bg service

--- src/app/services/get-bg.service.ts
+++ src/app/services/get-bg.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class GetBgService {
-  getBgUrl = 'http://piky.herokuapp.com/jessica';
+  getBgUrl = 'https://piky.herokuapp.com/jessica';
   constructor(private http : HttpClient) { }
   getBg(): Observable<BgArray>{
     return this.http.get<BgArray>(this.getBgUrl);
